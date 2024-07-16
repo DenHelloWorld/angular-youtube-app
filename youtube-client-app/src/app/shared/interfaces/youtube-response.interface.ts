97cce52af6313cb7583aa850e9a7f0a295e1e22d
Denis Saveliev <denissaveliev23@gmail.com>
2024-07-16 18:51:37 +0300

    refactor: decomposition interfaces

--- youtube-client-app/src/app/shared/interfaces/youtube-response.interface.ts
+++ youtube-client-app/src/app/shared/interfaces/youtube-response.interface.ts
@@ -1,49 +1,11 @@
-export interface Thumbnail {
-  url: string;
-  width: number;
-  height: number;
-}
-export interface Item {
-  kind: string;
-  etag: string;
-  id: string;
-  snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: {
-      default: Thumbnail;
-      medium: Thumbnail;
-      high: Thumbnail;
-      standard?: Thumbnail;
-      maxres?: Thumbnail;
-    };
-    channelTitle: string;
-    tags?: string[];
-    categoryId: string;
-    liveBroadcastContent: string;
-    localized: {
-      title: string;
-      description: string;
-    };
-    defaultAudioLanguage?: string;
-  };
-  statistics?: {
-    viewCount: string;
-    likeCount: string;
-    dislikeCount: string;
-    favoriteCount: string;
-    commentCount: string;
-  };
-}
+import { YouTubeVideoData } from './youtube-video-data.interface';
 
-export interface YouTubeVideoListResponse {
+export interface YouTubeResponse {
   kind: string;
   etag: string;
   pageInfo: {
     totalResults: number;
     resultsPerPage: number;
   };
-  items: Item[];
+  items: YouTubeVideoData[];
 }
