ee7afa630c02785eb238967458a395f2c4113fe9
Denis Saveliev <denissaveliev23@gmail.com>
2024-07-25 07:52:41 +0300

    chore: refresh api key

--- youtube-client-app/src/app/features/youtube/constants/api-key.ts
+++ youtube-client-app/src/app/features/youtube/constants/api-key.ts
@@ -1,1 +1,5 @@
-export const API_KEY: string = 'AIzaSyDnOEGL48M32jWSq6zBSR7CBXvL0O5zUus';
+// export const API_KEY: string = 'AIzaSyDnOEGL48M32jWSq6zBSR7CBXvL0O5zUus';
+
+// export const API_KEY: string = 'AIzaSyD8ydX4bpuLl5bKRnzKYm4HVfRPJvKka0Q';
+
+export const API_KEY: string = 'AIzaSyBgfGpW_mURy_uXNkNLn6vqMJfyvmRmpdo';
