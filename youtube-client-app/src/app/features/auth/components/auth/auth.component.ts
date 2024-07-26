f6686f8fbd1fcceb66554e5611b096f373152732
Denis Saveliev <denissaveliev23@gmail.com>
2024-07-26 04:53:25 +0300

    feat: add basic validation of login

--- youtube-client-app/src/app/features/auth/components/auth/auth.component.ts
+++ youtube-client-app/src/app/features/auth/components/auth/auth.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'app/features/auth/services/auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent {
-
-  public loginForm: FormGroup = this.formBuilder.group({
-    username: [''],
-    password: [''],
-  });
+export class AuthComponent implements OnInit {
+  public loginForm!: FormGroup;
 
   constructor(
     public authService: AuthService,
     private formBuilder: FormBuilder,
   ) {}
+
+  public ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: [''],
+    });
+  }
 
   public onSubmit() {
     const { username, password } = this.loginForm.value;
